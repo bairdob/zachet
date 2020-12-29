6ffbf1f2d14f92c1944adb8a96a30b722a732d47
Bair <krumane@mail.ru>
2020-12-29 16:00:30 +0300

    add

--- example20.cpp
+++ example20.cpp
@@ -1,31 +1,28 @@
 //example20.cpp
 #include <iostream>
+#include <memory>
 using namespace std;
 
-void one(int a){ //передача по значению
-	cout << a << endl;
-}
-void two(int &a){ //передача по ссылке позволяет изменять объект
-	a = 1; 		
-	cout << a << endl;
-}
-void three(int *a){ //передача по указателю аналогично Си
-	*a = 2; 		
-	cout << *a << endl;
-}
-void four(const int &a){
-	//a = 3; строчка не скомпилируются тк в параметре передачи const
-	cout << a << endl;
-}
+class Example{
+private:
+	int data;
+public:
+	Example(int n){
+		data = n;
+		cout << "Created" << endl;
+	}
+	~Example(){
+		cout << "Deleted" << endl;
+	}
+	void show(){
+		cout << data << endl;
+	}
+
+};
+
 
 int main(){
-	int a = 0;
-	one(a);
-	two(a);
-	cout << a << endl;
-	three(&a);
-	cout << a << endl;
-	int b = 3;
-	four(b);
+	shared_ptr<Example> ps (new Example(3));
+	ps->show();
 	return 0;
 }
